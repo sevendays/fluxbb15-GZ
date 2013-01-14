a96006eba8ee3f4b30518d85e471e4faf666682a
seven <zamboluca@gmail.com>
2013-01-14 13:25:36 +0100

    Update to FluxBB 1.5.2
    Merged FluxBB 1.5.2 files, keeping PHPass, Akismet and the modified user registration.

--- register.php
+++ register.php
@@ -66,7 +66,7 @@ $errors = array();
 if (isset($_POST['form_sent']))
 {
 	// Check that someone from this IP didn't register a user within the last hour (DoS prevention)
-	$result = $db->query('SELECT 1 FROM '.$db->prefix.'users WHERE registration_ip=\''.get_remote_address().'\' AND registered>'.(time() - 3600)) or error('Unable to fetch user info', __FILE__, __LINE__, $db->error());
+	$result = $db->query('SELECT 1 FROM '.$db->prefix.'users WHERE registration_ip=\''.$db->escape(get_remote_address()).'\' AND registered>'.(time() - 3600)) or error('Unable to fetch user info', __FILE__, __LINE__, $db->error());
 
 	if ($db->num_rows($result))
 		message($lang_register['Registration flood']);
